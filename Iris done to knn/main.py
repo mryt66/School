caabc8b44e7d7115a5bd03cc37be0ee2027f296d
Marcin <64143856+mryt66@users.noreply.github.com>
2023-04-06 15:33:55 +0200

    Add files via upload

---
+++ Iris done to knn/main.py
@@ -0,0 +1,84 @@
+import pandas
+import seaborn
+import random
+from decimal import Decimal
+from math import sqrt
+
+
+class DataProcessing:
+    data = None
+
+    def __init__(self, d):
+        self.data = d
+        pandas.set_option('display.max_rows', len(self.data))
+
+    def shuffle(self):
+        for i in range(len(self.data) - 1, 0, -1):
+            j = random.randint(0, i)
+            self.data.iloc[i], self.data.iloc[j] = self.data.iloc[j], self.data.iloc[i]
+        return self.data
+
+    def normalize(self):
+        listCopy = self.data.copy()
+        values = listCopy.select_dtypes(exclude="object")
+        columnNames = values.columns.tolist()
+        for column in columnNames:
+            data = listCopy.loc[:, column]
+            minimum, maximum = min((data)), max(data)
+            for row in range(0, len(listCopy), 1):
+                value = (listCopy.at[row, column] - minimum) / (maximum - minimum)
+                listCopy.at[row, column] = value
+        self.data = listCopy
+
+    def split(self):
+        split = int(len(self.data) * 0.7)
+        listTrain = self.data.iloc[:split, :]
+        listVal = self.data.iloc[split:, :]
+        return listTrain, listVal
+
+
+class co:
+    def __init__(self, list1):
+        self.list1 = list1
+        l1 = self.list1
+        self.list1 = l1.iloc[:, :-1] #self.list1 = l1.iloc[:, :-1]
+
+
+    def euklides(self):
+        euk2 = []
+        for i in self.list1.iloc: #for i in self.list1.iloc:
+            euk = []
+            for j in self.list1.iloc:
+                euk.append([j.name,sqrt((Decimal(i[0]) - Decimal(j[0])) ** 2 + (Decimal(i[1]) - Decimal(j[1])) ** 2 + (
+                    Decimal(i[2]) - Decimal(j[2])) ** 2 + (Decimal(i[3]) - Decimal(j[3])) ** 2)])
+            sorting(euk)
+            euk2.append([i.name,euk[0][0],euk[1][0],euk[2][0],euk[3][0],euk[4][0]]) # 5 nearest neighbours
+
+        print(euk2)
+
+
+def sorting(euk): #Reversed sorting with index
+    for i in range(1,len(euk)):
+        for j in range(1,len(euk)):
+            if euk[j-1][1]<euk[j][1]:
+                tmp=euk[j-1]
+                euk[j-1]=euk[j]
+                euk[j]=tmp
+    return euk
+
+data = pandas.read_csv('iris.csv')
+data.describe()
+seaborn.pairplot(data, hue="variety")
+
+x = DataProcessing(data)
+x.shuffle()
+x.normalize()
+training, values = x.split()
+# for i in training:
+#     print(i)
+newx=co(training)
+print(newx.euklides())
+l1=[[4,3434],[3,345],[2,34],[5,354435],[1,5]]
+l1=sorting(l1)
+print(l1)
+# newx.euklides()
