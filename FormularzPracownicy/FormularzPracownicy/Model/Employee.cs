321fa90a9797fcc74c0daa4fe582a1dc7117ccee
Marcin <64143856+mryt66@users.noreply.github.com>
2023-05-02 16:43:52 +0200

    Pracownicy MVVM windows forms

---
+++ FormularzPracownicy/FormularzPracownicy/Model/Employee.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormularzPracownicy.Model
+{
+    public enum Position {Lekarz, Fizyk, Akrobata, Malarz, Tester};
+    public enum TypeOfContract {prace, b2b, zlecenie};
+
+    public class Employee
+    {
+        private String name1;
+        private String surname1;
+        private DateTime birthdate1;
+        private decimal salary1;
+        private Position position1;
+        private TypeOfContract contract1;
+
+        public Employee() { }
+
+        public Employee(string name, string surname, DateTime birthdate, decimal salary, Position position, TypeOfContract contract)
+        {
+            name1 = name;
+            surname1 = surname;
+            birthdate1 = birthdate;
+            salary1 = salary;
+            position1 = position;
+            contract1 = contract;
+        }
+
+        public string Name
+        {
+            get { return name1; }
+            set { name1 = value; }
+        }
+
+        public string Surname
+        {
+            get { return surname1; }
+            set { surname1 = value; }
+        }
+
+        public DateTime Birthdate
+        {
+            get { return birthdate1; }
+            set { birthdate1 = value; }
+        }
+
+        public decimal Salary
+        {
+            get { return salary1; }
+            set  { salary1 = value; }
+        }
+
+        public TypeOfContract Contract
+        {
+            get { return contract1; }
+            set { contract1 = value; }
+        }
+
+        public Position Position
+        {
+            get { return position1; }
+            set { position1 = value; }
+        }
+
+        public override string ToString()
+        {
+
+            string type_of_contract = "";
+            string position = "";    
+
+            if (position1 == Position.Lekarz)
+            {
+                position = "Lekarz";
+            }
+            if (position1 == Position.Fizyk)
+            {
+                position = "Fizyk";
+            }
+            if (position1 == Position.Akrobata)
+            {
+                position = "Akrobata";
+            }
+            if (position1 == Position.Malarz)
+            {
+                position = "Malarz";
+            }
+            if (position1 == Position.Tester)
+            {
+                position = "Tester";
+            }
+
+            if (contract1 == TypeOfContract.prace)
+            {
+                type_of_contract = "Umowa o pracę";
+            }
+            if (contract1 == TypeOfContract.b2b)
+            {
+                type_of_contract = "B2B";
+            }
+            if (contract1 == TypeOfContract.zlecenie)
+            {
+                type_of_contract = "Umowa zlecenie";
+            }
+
+            return $" {name1} {surname1} ; Data urodzenia: {birthdate1.ToShortDateString().ToString()} ; "  +
+                $"Stanowisko: {position} ; Rodzaj umowy: {type_of_contract} ; Pensja: {salary1} zł ";
+        }
+    }
+}
---
+++ FormularzPracownicy/FormularzPracownicy/Presenter/MainPresenter.cs
@@ -0,0 +1,174 @@
+﻿using FormularzPracownicy.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormularzPracownicy.Presenter
+{
+    class MainPresenter
+    {
+
+        private View.Form1 view1;
+        private Model.Employee tmpEmployee;
+        private Model.MainModel model1;
+
+        public MainPresenter(View.Form1 view, Model.MainModel model)
+        {
+            view1 = view;
+            model1 = model;
+
+            tmpEmployee = new Model.Employee("","",DateTime.Today,4000,Position.Lekarz,TypeOfContract.prace);
+
+            view1.ChangeName += _view_ChangeName;
+            view1.ChangeSurname += _view_ChangeSurname;
+            view1.ChangeDate += _view_ChangeDate;
+            view1.ChangeSalary += _view_ChangeSalary;
+            view1.ChangePosition += _view_ChangePosition;
+            view1.ChangeContract += _view_ChangeContract;
+            view1.AddPerson += _view_AddPerson;
+            view1.SerializeList += _view_SerializeList;
+            view1.OnEmployeeSelect += _view_OnEmployeeSelect;
+            view1.DeserializeList += _view_DeserializeList;
+
+        }
+
+        private void _view_ChangeName(string name)
+        {
+            tmpEmployee.Name = name;
+        }
+
+        private void _view_ChangeSurname(string surname)
+        {
+            tmpEmployee.Surname = surname;
+        }
+
+        private void _view_ChangeDate(DateTime date)
+        {
+            tmpEmployee.Birthdate = date;
+        }
+
+        private void _view_ChangeSalary(decimal salary)
+        {
+            tmpEmployee.Salary = salary;
+        }
+
+        private void _view_ChangePosition(int position_id)
+        {
+            if (position_id == 0)
+            {
+                tmpEmployee.Position = Model.Position.Lekarz;
+            }
+
+            else if (position_id == 1)
+            {
+                tmpEmployee.Position = Model.Position.Fizyk;
+            }
+
+            else if (position_id == 2)
+            {
+                tmpEmployee.Position = Model.Position.Akrobata;
+            }
+
+            else if (position_id == 3)
+            {
+                tmpEmployee.Position = Model.Position.Malarz;
+            }
+
+            else if (position_id == 4)
+            {
+                tmpEmployee.Position = Model.Position.Tester;
+            }
+        }
+
+        private void _view_ChangeContract(int contract_id)
+        {
+            if (contract_id == 0)
+            {
+                tmpEmployee.Contract = Model.TypeOfContract.prace;
+            }
+
+            else if (contract_id == 1)
+            {
+                tmpEmployee.Contract = Model.TypeOfContract.b2b;
+            }
+
+            else if (contract_id == 2)
+            {
+                tmpEmployee.Contract = Model.TypeOfContract.zlecenie;
+            }
+        }
+
+        private void _view_AddPerson()
+        {
+            model1.AddEmployee(tmpEmployee);
+            tmpEmployee = new Model.Employee("", "", DateTime.Today, 4000, Position.Lekarz, TypeOfContract.prace);
+            updateView();
+        }
+
+        private void updateView()
+        {
+            view1.DisplayEmployees.Clear();
+            foreach (Employee e in model1.getList())
+            {
+                view1.DisplayEmployees.Add(e.ToString());
+            }
+        }
+
+        private void _view_SerializeList()
+        {
+            Console.WriteLine("Serializacja");
+            model1.SerializeList(model1.getList());
+        }
+
+        private void _view_DeserializeList()
+        {
+            Console.WriteLine("Deserializacja");
+            model1.DeserializeList();
+            updateView();
+        }
+
+        private void _view_OnEmployeeSelect(int index)
+        {
+
+            tmpEmployee = model1.getList()[index];
+            int position = 0;
+            if (tmpEmployee.Position == Position.Lekarz) { position = 0; }
+            else if (tmpEmployee.Position == Position.Fizyk) { position = 1; }
+            else if (tmpEmployee.Position == Position.Akrobata) { position = 2; }
+            else if (tmpEmployee.Position == Position.Malarz) { position = 3; }
+            else if (tmpEmployee.Position == Position.Tester) { position = 4; }
+
+            bool r1 = true;
+            bool r2 = false;
+            bool r3 = false;
+
+            if (tmpEmployee.Contract == TypeOfContract.prace) 
+            {
+
+                r1 = true;
+                r2 = false;
+                r3 = false;
+
+            }
+
+            else if (tmpEmployee.Contract == TypeOfContract.b2b)
+            {
+                r1 = false;
+                r2 = true;
+                r3 = false;
+            }
+
+            else if (tmpEmployee.Contract == TypeOfContract.zlecenie) 
+            {
+                r1 = false;
+                r2 = false;
+                r3 = true;
+            }
+
+            view1.setFormValues(tmpEmployee.Name, tmpEmployee.Surname, tmpEmployee.Birthdate, tmpEmployee.Salary, position, r1, r2, r3);
+        }
+    }
+}
